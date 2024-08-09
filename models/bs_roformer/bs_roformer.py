943b2f1264627e40f835dcf9c58547fcb79277f0
bfloat16 <a1004428886@126.com>
2024-08-10 01:13:33 +0800

    update

--- models/bs_roformer/bs_roformer.py
+++ models/bs_roformer/bs_roformer.py
@@ -204,7 +204,6 @@ class BSRoformer(Module):
             num_stems=1,
             time_transformer_depth=2,
             freq_transformer_depth=2,
-            linear_transformer_depth=0,
             freqs_per_bands,
             dim_head=64,
             heads=8,
@@ -235,8 +234,6 @@ class BSRoformer(Module):
 
         for _ in range(depth):
             tran_modules = []
-            if linear_transformer_depth > 0:
-                tran_modules.append(Transformer(depth=linear_transformer_depth, linear_attn=True, **transformer_kwargs))
             tran_modules.append(Transformer(depth=time_transformer_depth, rotary_embed=time_rotary_embed, **transformer_kwargs))
             tran_modules.append(Transformer(depth=freq_transformer_depth, rotary_embed=freq_rotary_embed, **transformer_kwargs))
             self.layers.append(nn.ModuleList(tran_modules))
@@ -271,14 +268,7 @@ class BSRoformer(Module):
         x = self.band_split(x)
 
         for transformer_block in self.layers:
-            if len(transformer_block) == 3:
-                linear_transformer, time_transformer, freq_transformer = transformer_block
-
-                x, ft_ps = pack([x], 'b * d')
-                x = linear_transformer(x)
-                x, = unpack(x, ft_ps, 'b * d')
-            else:
-                time_transformer, freq_transformer = transformer_block
+            time_transformer, freq_transformer = transformer_block
 
             x = rearrange(x, 'b t f d -> b f t d')
             x, ps = pack([x], '* t d')
@@ -295,11 +285,7 @@ class BSRoformer(Module):
 
         x = self.final_norm(x)
 
-        num_stems = torch.tensor(len(self.mask_estimators))
-
         mask = torch.stack([fn(x) for fn in self.mask_estimators], dim=1)
         mask = rearrange(mask, 'b n t (f c) -> b n f t c', c=2)
 
-        print(mask.shape, num_stems.shape)
-
-        return mask, num_stems
+        return mask
